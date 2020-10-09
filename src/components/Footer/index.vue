fa4d53e03b5784f69f91c723ee42a76f52ca23d1
wallbanger1 <easyspeak@protonmail.com>
2020-10-08 19:21:29 -0700

    ...

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -19,24 +19,6 @@
   </div>
 
 </div>
-<!-- 
-  <div class="navbar is-fixed-bottom footr">
-
-    <div class="columns is-vcentered">
-
-      <div class="column is-half has-text-left red">
-        <a href="https://t.me/devotoken" target="_blank">
-          <img id="" width="40px" class="tele-logo" src="@/assets/telegramlogo.png" />
-        </a>
-      </div>
-
-      <div class="column is-6 has-text-right is-vcentered yellow">
-        <div class=""> &copy; 2020 DEVO</div>
-      </div>
-
-    </div>
-
-  </div> -->
 
 </template>
 
